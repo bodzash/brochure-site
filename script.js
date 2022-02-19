f0b5cae442b352954507dde120fbb12336c9c37f
bodzash <98101027+bodzash@users.noreply.github.com>
2022-02-19 09:47:00 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let btn = document.querySelector(".nav-btn")
 let menu = document.querySelector(".drop-down-wrapper")
 
-btn.onclick = ()=> {
-        menu.classList.toggle("nav-show")
-        menu.style.opacity = "1"
-}
+let shit = document.querySelectorAll(".drop-down-item")
+
+shit.forEach(item=> {
+        item.addEventListener("click",()=>{
+                menu.setAttribute("data-show","false")
+        })
+})
+
+btn.addEventListener("click",()=> {
+        const vis = menu.getAttribute("data-show")
+        vis === "true" ? menu.setAttribute("data-show","false") : menu.setAttribute("data-show","true")
+})
